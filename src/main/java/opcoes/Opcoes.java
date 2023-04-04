f8e9cc730ea00d9dbd45f2bfc17c6e46de2ba426
LeoTulini <tulini.leo@hotmail.com>
2023-04-04 19:10:11 -0300

    Refatorar opcoes

--- src/main/java/opcoes/Opcoes.java
+++ src/main/java/opcoes/Opcoes.java
@@ -6,65 +6,89 @@ import sala.Mapa;
 import java.util.Scanner;
 
 public class Opcoes {
-    protected Mapa mapa;
+    private Mapa mapa;
+    private int filaEscolhida;
+    private int cadeiraEscolhida;
 
     public Opcoes(Mapa mapa) {
         this.mapa = mapa;
     }
 
     public void reservar() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a letra da fileira desejada:");
-        int fila = (int) sc.next().toUpperCase().charAt(0) - 'A';
-        System.out.println("Digite o número da cadeira desejada:");
-        int cadeira = sc.nextInt() - 1;
+        this.receberFilaEcadeira();
+        this.validarFilaEcadeira();
 
         Assento[][] assentos = mapa.getAssentos();
-        if (assentos[fila][cadeira].isOcupado()) {
-            System.out.println("O assento " + (char) (fila + 'A') + (cadeira + 1) + " já está ocupado!");
+        if (assentos[filaEscolhida][cadeiraEscolhida].isOcupado()) {
+            System.out.println("O assento "
+                    + (char) (filaEscolhida + 'A')
+                    + (cadeiraEscolhida + 1)
+                    + " já está ocupado!");
 
         } else {
-            assentos[fila][cadeira].reservar();
-            System.out.println("O assento " + (char) (fila + 'A') + (cadeira + 1) + " reservado com sucesso!");
-
+            assentos[filaEscolhida][cadeiraEscolhida].reservar();
+            System.out.println("O assento "
+                    + (char) (filaEscolhida + 'A')
+                    + (cadeiraEscolhida + 1)
+                    + " reservado com sucesso!");
         }
     }
 
     public void cancelar() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a letra da fileira desejada:");
-        int fila = (int) sc.next().toUpperCase().charAt(0) - 'A';
-        System.out.println("Digite o número da cadeira desejada:");
-        int cadeira = sc.nextInt() - 1;
+        this.receberFilaEcadeira();
+        this.validarFilaEcadeira();
 
         Assento[][] assentos = mapa.getAssentos();
-        if (assentos[fila][cadeira].isOcupado()) {
-            assentos[fila][cadeira].cancelar();
+        if (assentos[filaEscolhida][cadeiraEscolhida].isOcupado()) {
+            assentos[filaEscolhida][cadeiraEscolhida].cancelar();
             System.out.println("Reserva cancelada com sucesso!");
         } else {
-            System.out.println("O assento " + (char) (fila + 'A') + (cadeira + 1) + " já está livre!");
+            System.out.println("O assento "
+                    + (char) (filaEscolhida + 'A')
+                    + (cadeiraEscolhida + 1)
+                    + " já está livre!");
 
         }
     }
 
     public void verificarDisponibilidade() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a letra da fileira desejada:");
-        int fila = (int) sc.next().toUpperCase().charAt(0) - 'A';
-        System.out.println("Digite o número da cadeira desejada:");
-        int cadeira = sc.nextInt() - 1;
+        this.receberFilaEcadeira();
+        this.validarFilaEcadeira();
 
         Assento[][] assentos = mapa.getAssentos();
-        if (assentos[fila][cadeira].isOcupado()) {
-            System.out.println("O assento " + (char) (fila + 'A') + (cadeira + 1) + " já está ocupado!");
+        if (assentos[filaEscolhida][cadeiraEscolhida].isOcupado()) {
+            System.out.println("O assento "
+                    + (char) (filaEscolhida + 'A')
+                    + (cadeiraEscolhida + 1)
+                    + " já está ocupado!");
 
         } else {
-            System.out.println("O assento " + (char) (fila + 'A') + (cadeira + 1) + " está disponível!");
-
+            System.out.println("O assento "
+                    + (char) (filaEscolhida + 'A')
+                    + (cadeiraEscolhida + 1)
+                    + " está disponível!");
         }
     }
 
     public void mostrarMapa() {
         mapa.imprimir();
+    }
+
+    private void validarFilaEcadeira(){
+        if(this.filaEscolhida > 12 || this.filaEscolhida < 1){
+            //Criar excecao
+        }
+
+        if(this.cadeiraEscolhida > 14 || this.cadeiraEscolhida < 1){
+
+        }
+    }
+
+    private void receberFilaEcadeira(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a letra da fileira desejada:");
+        this.filaEscolhida = sc.next().toUpperCase().charAt(0) - 'A';
+        System.out.println("Digite o número da cadeira desejada:");
+        this.cadeiraEscolhida = sc.nextInt() - 1;
     }
 }
